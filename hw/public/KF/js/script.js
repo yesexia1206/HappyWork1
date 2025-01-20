a84cf765e0f852191ff2c1205526f838eb498c98
yesexia1206 <yaya871206mike@gmail.com>
2025-01-20 22:57:02 +0800

    upd-2025-01-20-1

--- hw/public/KF/js/script.js
+++ hw/public/KF/js/script.js
@@ -30,54 +30,54 @@
 
 // http://jsfiddle.net/1893foc7/4/
 
-const circle = document.querySelector('.circle-button');
-const buttonText = document.querySelector('.circle-button .circle-button__text');
+// const circle = document.querySelector('.circle-button');
+// const buttonText = document.querySelector('.circle-button .circle-button__text');
 
-const toggleCircleAnimation = () => {
-    circle.classList.toggle('run-enter');
-    circle.classList.toggle('run-leave');
-}
+// const toggleCircleAnimation = () => {
+//     circle.classList.toggle('run-enter');
+//     circle.classList.toggle('run-leave');
+// }
 
-const handleButtonHover = () => {
-    buttonText.innerHTML = 'Play';
-}
+// const handleButtonHover = () => {
+//     buttonText.innerHTML = 'Play';
+// }
 
-const handleButtonOut = () => {
-    buttonText.innerHTML = 'The Story';
-}
+// const handleButtonOut = () => {
+//     buttonText.innerHTML = 'The Story';
+// }
 
-circle.addEventListener('mouseover', toggleCircleAnimation);
-circle.addEventListener('mouseover', handleButtonHover);
-circle.addEventListener('mouseout', toggleCircleAnimation);
-circle.addEventListener('mouseout', handleButtonOut);
-circle.addEventListener('click', () => console.log('circle click'));
+// circle.addEventListener('mouseover', toggleCircleAnimation);
+// circle.addEventListener('mouseover', handleButtonHover);
+// circle.addEventListener('mouseout', toggleCircleAnimation);
+// circle.addEventListener('mouseout', handleButtonOut);
+// circle.addEventListener('click', () => console.log('circle click'));
 
-const circleBackButton = document.querySelector('.circle-back-button');
+const circleBackButton = document.querySelector('.circle-back-button');//
 
 const toggleCircleBackAnimation = () => {
-    circleBackButton.classList.toggle('run-enter');
+    circleBackButton.classList.toggle('run-enter');//
 }
 
-circleBackButton.addEventListener('mouseover', toggleCircleBackAnimation);
-circleBackButton.addEventListener('mouseout', toggleCircleBackAnimation);
-circleBackButton.addEventListener('click', () => console.log('circle click'));
+// circleBackButton.addEventListener('mouseover', toggleCircleBackAnimation);
+// circleBackButton.addEventListener('mouseout', toggleCircleBackAnimation);
+// circleBackButton.addEventListener('click', () => console.log('circle click'));
 
-const circleForwardButton = document.querySelector('.circle-forward-button');
+// const circleForwardButton = document.querySelector('.circle-forward-button');
 
 const toggleCircleForwardAnimation = () => {
-    circleForwardButton.classList.toggle('run-enter');
+    circleForwardButton.classList.toggle('run-enter');//
 }
 
-circleForwardButton.addEventListener('mouseover', toggleCircleForwardAnimation);
-circleForwardButton.addEventListener('mouseout', toggleCircleForwardAnimation);
+circleForwardButton.addEventListener('mouseover', toggleCircleForwardAnimation);//
+circleForwardButton.addEventListener('mouseout', toggleCircleForwardAnimation);//
 
-const rect = document.querySelector('.rect-button');
+// const rect = document.querySelector('.rect-button');
 
-const toggleRectAnimation = () => {
-    rect.classList.toggle('run-enter');
-    rect.classList.toggle('run-leave');
-}
+// const toggleRectAnimation = () => {
+//     rect.classList.toggle('run-enter');
+//     rect.classList.toggle('run-leave');
+// }
 
-rect.addEventListener('mouseover', toggleRectAnimation);
-rect.addEventListener('mouseout', toggleRectAnimation);
-rect.addEventListener('click', () => console.log('rect click'));
+// rect.addEventListener('mouseover', toggleRectAnimation);
+// rect.addEventListener('mouseout', toggleRectAnimation);
+// rect.addEventListener('click', () => console.log('rect click'));
